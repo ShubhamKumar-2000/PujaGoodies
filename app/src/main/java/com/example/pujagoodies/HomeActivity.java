0c7a22c77852a366e4bd2b5d3046d63647323ff1
ShubhamKumar-2000 <kumarshuvam@gmail.com>
2023-02-09 09:41:30 +0530

    first everything woking commit implemented Google signin and grid view

---
+++ app/src/main/java/com/example/pujagoodies/HomeActivity.java
@@ -0,0 +1,76 @@
+package com.example.pujagoodies;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.developer.gbuttons.GoogleSignInButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+//    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth auth;
+    GoogleSignInButton googleBtn;
+    GoogleSignInOptions gOptions;
+    GoogleSignInClient gClient;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        getSupportActionBar().hide();
+        googleBtn = findViewById(R.id.googleBtn);
+        auth=FirebaseAuth.getInstance();
+
+        gOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gClient = GoogleSignIn.getClient(this, gOptions);
+        GoogleSignInAccount gAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if(gAccount != null){
+            finish();
+            Intent intent = new Intent(HomeActivity.this,LandingActivity.class);
+            startActivity(intent);
+        }
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode() == Activity.RESULT_OK){
+                            Intent data = result.getData();
+                            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                            try {
+                                task.getResult(ApiException.class);
+                                finish();
+                                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }catch (ApiException e){
+                                Toast.makeText(HomeActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = gClient.getSignInIntent();
+                activityResultLauncher.launch(signInIntent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/pujagoodies/LandingActivity.java
@@ -0,0 +1,108 @@
+package com.example.pujagoodies;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Outline;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewOutlineProvider;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.pujagoodies.databinding.ActivityLandingBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import org.jetbrains.annotations.NotNull;
+
+public class LandingActivity extends AppCompatActivity {
+    TextView userName;
+    Button logout;
+    GoogleSignInClient gClient;
+    GoogleSignInOptions gOptions;
+    ActivityLandingBinding binding;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLandingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        String[] name= { "Aastha"
+                , "Aniruddhacharya ji"
+                , "Satsang"
+                , "Bhakthi TV"
+                , "Shri Devkinandan Thakur Ji"
+                , "Pandit Pradeep Ji Mishra Sehore Wale"
+                , "Bhakti Sangeet HDN"
+                , "JayaKishori Motivation"
+                , "Iamjayakishori"
+                , "SanskarTV"
+                , "SADHNA GOLD"
+                , "Saregama Bhakti"
+                , "Gauri Gopal Tv"
+                , "Swami Raghvacharya"
+                , "BhaktiSagar Tv"
+                , "Shemaroo Bhakti"
+                , "T-Series Bhakti Sagar"
+                , "Pen Bhakti"};
+
+        int[] image = {R.drawable.aastha, R.drawable.aniruddhacharyaji, R.drawable.satsang, R.drawable.bhakthitv, R.drawable.shridevkinandan,
+                R.drawable.panditpradeepji, R.drawable.bhaktisangeet, R.drawable.jayakishori, R.drawable.iamjayakishori, R.drawable.sanskartv,
+                R.drawable.sadhnagold, R.drawable.saregamabhakti, R.drawable.gaurigopaltv, R.drawable.swamiraghvacharya, R.drawable.bhaktisagartv,
+                R.drawable.shemaroobhakti, R.drawable.tseriesbhaktisagar, R.drawable.penbhakti};
+
+        // this makes the grid items circular
+        //ends here
+
+        GridAdapter gridAdapter = new GridAdapter(LandingActivity.this, name,image);
+        binding.gridView.setAdapter(gridAdapter);
+
+        binding.gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //define your Logic and Intent here
+                Toast.makeText(LandingActivity.this, "You Clikced"+name[position], Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        getSupportActionBar().hide();
+        logout = findViewById(R.id.logout);
+//        userName = findViewById(R.id.userName);
+
+        gOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gClient = GoogleSignIn.getClient(this, gOptions);
+
+        GoogleSignInAccount gAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if(gAccount != null){
+            String gName = gAccount.getDisplayName();
+//            userName.setText(gName);
+        }
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        finish();
+                        startActivity(new Intent(LandingActivity.this, HomeActivity.class));
+                    }
+                });
+            }
+        });
+    }
+}
